d19de489414260966d290531db71d6291b906c60
KEVIN ANDREW A <157099606+Andrew-Kevin-007@users.noreply.github.com>
2025-10-15 15:12:53 +0530

    Update image path in resize.py

--- resize.py
+++ resize.py
@@ -1,8 +1,9 @@
 import cv2
-img = cv2.imread('assets/CR7.jpg')
+img = cv2.imread('#path')
 
 def resize_img(image, scale=0.5):
     width = int(image.shape[1] * scale)
     height = int(image.shape[0] * scale)
     dimensions = (width, height)
     return cv2.resize(image, dimensions, interpolation=cv2.INTER_AREA)
+
