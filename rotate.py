3eb57f642993dca3ea61979e9d0626a716cfb9bf
KEVIN ANDREW A <157099606+Andrew-Kevin-007@users.noreply.github.com>
2025-10-15 15:10:22 +0530

    Update image path to use variable '#imgpath'

--- rotate.py
+++ rotate.py
@@ -1,7 +1,8 @@
 import cv2
-img = cv2.imread('assets/CR7.jpg')
+img = cv2.imread('#imgpath')
 cv2.imshow('image', img)
 rotated = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
 cv2.imshow('Rotated', rotated)          
 cv2.waitKey(0)
+
 cv2.destroyAllWindows()
