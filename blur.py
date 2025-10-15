6de735761379d90f7e15e6d70b927c98a44630e6
KEVIN ANDREW A <157099606+Andrew-Kevin-007@users.noreply.github.com>
2025-10-15 15:10:56 +0530

    Update image path to use variable #imgpath

--- blur.py
+++ blur.py
@@ -1,7 +1,8 @@
 import cv2
-img = cv2.imread('assets/CR7.jpg')
+img = cv2.imread('#imgpath')
 blur = cv2.GaussianBlur(img, (7,7), cv2.BORDER_DEFAULT)
 cv2.imshow('Blurred', blur)
 cv2.imshow('image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
