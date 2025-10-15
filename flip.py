950ad6c385236fdbb36ebc6f1f6ac788caefee94
KEVIN ANDREW A <157099606+Andrew-Kevin-007@users.noreply.github.com>
2025-10-15 15:11:25 +0530

    Change image path to variable in flip.py
    Updated image path to use a variable instead of a hardcoded string.

--- flip.py
+++ flip.py
@@ -1,9 +1,10 @@
 import cv2
-img = cv2.imread('assets/CR7.jpg')
+img = cv2.imread('#imgpath')
 cv2.imshow('CR7', img)
 flip_h = cv2.flip(img, 1)
 flip_v = cv2.flip(img, 0)   
 cv2.imshow('Flip Horizontal', flip_h)
 cv2.imshow('Flip Vertical', flip_v)
 cv2.waitKey(0)
+
 cv2.destroyAllWindows()
